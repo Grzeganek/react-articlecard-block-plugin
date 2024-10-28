a6919cf1703d1cd70c2e900ee26ad9e243b6539c
Grzeganek <74913313+Grzeganek@users.noreply.github.com>
2024-10-28 15:48:43 +0100

    Remove build folder from version control and add to .gitignore

--- build/ArtikelCard.tsx.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '4eb244a20cb76ad030fa');
--- build/render.php
+++
@@ -1,1 +0,0 @@
-<div class="update"><pre style="display: none;"><?php echo wp_json_encode($attributes) ?></pre></div>
